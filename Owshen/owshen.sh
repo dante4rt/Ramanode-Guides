1d7ad1ad84f3eb80b8ee64ba4b1729d550084a26
Dante4rt <rxmxdhxni@gmail.com>
2024-01-22 00:23:04 +0700

    fix: handle npm errors

--- Owshen/owshen.sh
+++ Owshen/owshen.sh
@@ -6,31 +6,48 @@ clear
 curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
 sleep 2
 
+
+echo "Updating packages..."
+sudo apt update
 echo "Installing Node.js and npm..."
-./loader.sh "sudo apt update && sudo apt install nodejs npm -y" "..." "Updating and installing Node.js and npm"
+sudo apt install nodejs npm -y
+echo "Node.js and npm installed."
+
 
 echo "Installing FUSE..."
-./loader.sh "sudo apt update && sudo apt install libfuse2 -y && sudo modprobe fuse && sudo groupadd fuse && user=\"$(whoami)\" && sudo usermod -a -G fuse \$user" "..." "Installing FUSE"
+sudo apt install libfuse2 -y
+sudo modprobe fuse
+sudo groupadd fuse 2>/dev/null 
+user="$(whoami)"
+sudo usermod -a -G fuse "$user"
+echo "FUSE installed."
+
 
 echo "Installing snarkjs..."
-./loader.sh "sudo npm install -g snarkjs" "..." "Installing snarkjs"
+sudo npm install -g snarkjs
+echo "snarkjs installed."
+
 
 echo "Downloading Owshen Wallet..."
-wget https://github.com/OwshenNetwork/owshen/releases/download/v0.1.3/Owshen_v0.1.3_x86_64.AppImage
+if [ ! -f Owshen_v0.1.3_x86_64.AppImage ]; then
+    wget https://github.com/OwshenNetwork/owshen/releases/download/v0.1.3/Owshen_v0.1.3_x86_64.AppImage
+fi
 chmod +x Owshen_v0.1.3_x86_64.AppImage
-./loader.sh "echo 'AppImage downloaded and made executable'" "..." "Preparing Owshen Wallet"
+echo "Owshen Wallet downloaded and made executable."
+
 
 initialize_wallet() {
     read -p "Enter your 12-word mnemonic phrase: " MNEMONIC
     ./Owshen_v0.1.3_x86_64.AppImage init --mnemonic "$MNEMONIC"
 }
 
+
 if [ ! -f ~/.owshen-wallet ]; then
     echo "Initializing Owshen Wallet..."
     initialize_wallet
 else
     echo "Owshen Wallet is already initialized."
-    read -p "Do you want to reinitialize your wallet? This will remove your current wallet files (y/n): " REINIT
+    read -p "Do you want to reinitialize your wallet? (y/n): " REINIT
     if [[ $REINIT =~ ^[Yy]$ ]]; then
         echo "Removing old wallet files..."
         rm -rf ~/.owshen-wallet
@@ -39,5 +56,6 @@ else
     fi
 fi
 
+
 echo "Running Owshen Wallet..."
-./loader.sh "./Owshen_v0.1.3_x86_64.AppImage wallet" "..." "Launching Owshen Wallet"
+./Owshen_v0.1.3_x86_64.AppImage wallet
