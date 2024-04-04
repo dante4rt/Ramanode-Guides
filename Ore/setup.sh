94f31d3c5fb15a8d7e60768919bef84e188ad2c8
Dante4rt <rxmxdhxni@gmail.com>
2024-04-04 12:14:45 +0700

    feat: add ORE

---
+++ Ore/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "Showing HCA logo..."
+wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
+curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
+sleep 2
+
+curl https://sh.rustup.rs -sSf | sh 
+. "$HOME/.cargo/env"
+
+sh -c "$(curl -sSfL https://release.solana.com/v1.18.4/install)"
+export PATH="/root/.local/share/solana/install/active_release/bin:$PATH"
+
+solana-keygen new 
+
+echo "Your Solana wallet address (public key):"
+solana-keygen pubkey 
+echo "Please deposit at least 0.101 SOL to this address."
+
+
+read -p "Have you deposited at least 0.101 SOL to the address? (y/n): " confirm_deposit
+if [ "$confirm_deposit" != "y" ]; then
+    echo "Please deposit at least 0.101 SOL to the address and try again."
+    exit 1
+fi
+
+sudo apt-get install -y build-essential gcc
+
+cargo install ore-cli
+
+read -p "Please enter the RPC URL: " rpc_url
+
+cat <<EOF > ore.sh
+ 
+while true 
+do 
+  echo "Running" 
+  ore --rpc "$rpc_url" --keypair ~/.config/solana/id.json --priority-fee 1000 mine --threads 4 
+  echo "Exited" 
+done 
+EOF
+
+chmod +x ore.sh
+
+./ore.sh
+
+echo "Mining process started. Check ore.sh for details."
