96826cacfbfdc3fcd24efbb01712e6fa233567e9
Dante4rt <rxmxdhxni@gmail.com>
2024-04-05 13:27:46 +0700

    fix: revert the code

--- Avail/avail.sh
+++ Avail/avail.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 
-show_hca_logo() {
-    echo "Showing HCA logo..."
-    wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
-    curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
-    sleep 2
-}
+echo "Showing HCA logo..."
+wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
+curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
+sleep 2
 
-remove_existing_avail_script() {
-    if [ -f "avail.sh" ]; then
-        echo "Removing existing avail.sh file..."
-        rm avail.sh
-    fi
-}
+if [ -f "avail.sh" ]; then
+    echo "Removing existing avail.sh file..."
+    rm avail.sh
+fi
 
-download_avail_script() {
-    echo "Downloading avail.sh script..."
-    wget -O avail.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/avail.sh
-}
+echo "Downloading avail.sh script..."
+wget -O avail.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/avail.sh
 
-setup_avail_service() {
-    sudo tee /etc/systemd/system/avail.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/avail.service > /dev/null <<EOF
 [Unit]
 Description=Avail Light Client
 After=network.target
@@ -37,24 +30,11 @@ LimitNOFILE=65000
 WantedBy=multi-user.target
 EOF
 
-    sudo systemctl daemon-reload
-    sudo systemctl enable avail
-    sudo systemctl start avail
-}
+sudo systemctl daemon-reload
+sudo systemctl enable avail
+sudo systemctl start avail
 
-check_logs() {
-    read -p "Do you want to check logs (y/n)? " choice
-    if [ "$choice" = "y" ]; then
-        sudo journalctl -fu avail -o cat
-    fi
-}
-
-main() {
-    show_hca_logo
-    remove_existing_avail_script
-    download_avail_script
-    setup_avail_service
-    check_logs
-}
-
-main
+read -p "Do you want to check logs (y/n)? " choice
+if [ "$choice" = "y" ]; then
+    sudo journalctl -fu avail -o cat
+fi
