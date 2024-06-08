17af50fa329933da9d60af0583ae3314437a7d44
Dante4rt <rxmxdhxni@gmail.com>
2024-06-09 02:35:12 +0700

    fix: make the code saver

--- Nubit/light-node.sh
+++ Nubit/light-node.sh
@@ -14,7 +14,8 @@ sudo apt install -y tmux
 sudo echo "deb http://security.ubuntu.com/ubuntu jammy-security main" >> /etc/apt/sources.list
 sudo apt -qy update && sudo apt -qy install libc6
 
-rm -rf nubit-node $HOME/.nubit-light-nubit-alphatestnet-1
+# rm -rf nubit-node $HOME/.nubit-light-nubit-alphatestnet-1
+rm -rf nubit-node
 
 tmux new -s nubit "curl -sL1 https://nubit.sh | bash"
 
