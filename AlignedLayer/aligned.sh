f147147b82ce2264cd3b2faa4e0fa07a36712ec2
Dante4rt <rxmxdhxni@gmail.com>
2024-06-18 20:50:59 +0700

    fix: add dynamic sources

--- AlignedLayer/aligned.sh
+++ AlignedLayer/aligned.sh
@@ -17,8 +17,38 @@ sudo apt-get install curl -y
 echo "Downloading and installing ALignedProof..."
 curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/install_aligned.sh | bash
 
-echo "Sourcing the .bashrc to update the environment variables..."
-source "$HOME/.bashrc"
+case $SHELL in
+*/zsh)
+    PROFILE="${ZDOTDIR-"$HOME"}/.zshenv"
+    PREF_SHELL=zsh
+    ;;
+*/bash)
+    PROFILE=$HOME/.bashrc
+    PREF_SHELL=bash
+    ;;
+*/fish)
+    PROFILE=$HOME/.config/fish/config.fish
+    PREF_SHELL=fish
+    ;;
+*/ash)
+    PROFILE=$HOME/.profile
+    PREF_SHELL=ash
+    ;;
+*)
+    echo "aligned: could not detect shell, manually add ${ALIGNED_BIN_DIR} to your PATH."
+    exit 1
+esac
+
+if [[ ":$PATH:" != *":${ALIGNED_BIN_DIR}:"* ]]; then
+    if [[ "$PREF_SHELL" == "fish" ]]; then
+        echo >> "$PROFILE" && echo "fish_add_path -a $ALIGNED_BIN_DIR" >> "$PROFILE"
+    else
+        echo >> "$PROFILE" && echo "export PATH=\"\$PATH:$ALIGNED_BIN_DIR\"" >> "$PROFILE"
+    fi
+fi
+
+echo "Sourcing the profile to update the environment variables..."
+source "$PROFILE"
 
 echo "Downloading an example SP1 proof file with its ELF file..."
 curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/get_proof_test_files.sh | bash
