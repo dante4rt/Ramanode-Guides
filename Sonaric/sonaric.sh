551f1ef667402da3896903c174876ffcb2e94501
Aprm <78480857+DiscoverMyself@users.noreply.github.com>
2024-06-28 17:56:10 +0700

    Update sonaric.sh

--- Sonaric/sonaric.sh
+++ Sonaric/sonaric.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 sudo apt-get update && sudo apt get upgrade -y
+clear
 
 wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
 curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
