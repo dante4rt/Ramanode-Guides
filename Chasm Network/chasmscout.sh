27ce1c507bb398c22aebb28787ff35e1cab87939
Aprm <78480857+DiscoverMyself@users.noreply.github.com>
2024-07-27 23:21:04 +0700

    Create chasmscout.sh

---
+++ Chasm Network/chasmscout.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
+curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
+sleep 4
+
+sudo apt-get update && sudo apt get upgrade -y
+clear
+
+read -p "Enter your Chasm Scout Name : " SCOUTNAME
+read -p "Enter your Scout UID: " SCOUTUID
+read -p "Enter your Webhook API Key: " WEBHOOKAPI
+read -p "Enter your Groq API: " GROQAPI
+
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+mkdir chasm
+cd chasm
+
+cat > .env <<EOF
+PORT=3001
+LOGGER_LEVEL=debug
+
+# Chasm
+ORCHESTRATOR_URL=https://orchestrator.chasm.net
+SCOUT_NAME=$SCOUTNAME
+SCOUT_UID=$SCOUTUID
+WEBHOOK_API_KEY=$WEBHOOKAPI
+# Scout Webhook Url, update based on your server's IP and Port
+# e.g. http://123.123.123.123:3001/
+WEBHOOK_URL=http://$(hostname -I | awk '{print $1}'):3001/
+
+# Chosen Provider (groq, openai)
+PROVIDERS=groq
+MODEL=gemma2-9b-it
+GROQ_API_KEY=$GROQAPI
+
+# Optional
+OPENROUTER_API_KEY=
+OPENAI_API_KEY=
+EOF
+
+docker pull chasmtech/chasm-scout:latest
+docker run -d --restart=always --env-file ./.env -p 3001:3001 --name scout chasmtech/chasm-scout
+
+echo "All Set! Subscribe: https://t.me/HappyCuanAirdrop"
+echo "Check your Scouts Here: https://scout.chasm.net/dashboard"
