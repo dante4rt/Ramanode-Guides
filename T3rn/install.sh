acc51799fc5f9f325e3fb62a1c43c335ad94dfc2
Dante4rt <rxmxdhxni@gmail.com>
2024-09-10 22:47:25 +0700

    fix: wrong callable url

--- T3rn/install.sh
+++ T3rn/install.sh
@@ -8,12 +8,12 @@ sleep 2
 echo "Welcome to the t3rn Executor Setup by Happy Cuan Airdrop!"
 
 cd $HOME
-rm -rf executor
+rm -rf executor; rm t3rn.sh
 sudo apt -q update
 sudo apt -qy upgrade
 
-EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.20.0/executor-linux-v0.21.0.tar.gz"
-EXECUTOR_FILE="executor-linux-v0.20.0.tar.gz"
+EXECUTOR_URL="https://github.com/t3rn/executor-release/releases/download/v0.21.0/executor-linux-v0.21.0.tar.gz"
+EXECUTOR_FILE="executor-linux-v0.21.0.tar.gz"
 
 echo "Downloading the Executor binary from $EXECUTOR_URL..."
 curl -L -o $EXECUTOR_FILE $EXECUTOR_URL
