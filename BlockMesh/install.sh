8e8182092c9de894507d3dda415c4ee9987cfb23
Dante4rt <rxmxdhxni@gmail.com>
2024-11-07 11:17:08 +0700

    chore (Blockmesh): update v

--- BlockMesh/install.sh
+++ BlockMesh/install.sh
@@ -5,13 +5,10 @@ wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guid
 curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
 sleep 2
 
-# Update and upgrade
 apt update && apt upgrade -y
 
-# Cleanup previous files
 rm -rf blockmesh-cli.tar.gz target
 
-# Install Docker if not installed
 if ! command -v docker &> /dev/null; then
     echo "Installing Docker..."
     apt-get install -y \
@@ -26,31 +23,25 @@ else
     echo "Docker is already installed, skipping..."
 fi
 
-# Install Docker Compose
 echo "Installing Docker Compose..."
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Create target directory for extraction
 mkdir -p target/release
 
-# Download and extract BlockMesh CLI
 echo "Downloading and extracting BlockMesh CLI..."
-curl -L https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.326/blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz -o blockmesh-cli.tar.gz
+curl -L https://github.com/block-mesh/block-mesh-monorepo/releases/download/v0.0.343/blockmesh-cli-x86_64-unknown-linux-gnu.tar.gz -o blockmesh-cli.tar.gz
 tar -xzf blockmesh-cli.tar.gz --strip-components=3 -C target/release
 
-# Verify extraction
 if [[ ! -f target/release/blockmesh-cli ]]; then
     echo "Error: blockmesh-cli binary not found in target/release. Exiting..."
     exit 1
 fi
 
-# Prompt for email and password
 read -p "Enter your BlockMesh email: " email
 read -s -p "Enter your BlockMesh password: " password
 echo
 
-# Run the Docker container with the BlockMesh CLI
 echo "Creating a Docker container for the BlockMesh CLI..."
 docker run -it --rm \
     --name blockmesh-cli-container \
